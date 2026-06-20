96eda9d77916021b8b5bb4a8a585e0b9494c8dc3
iamvirul <iamvirul@users.noreply.github.com>
2026-06-20 15:14:42 +0000

    deploy: 40965a5646884e9c9c6c332e8eb44f8311cce8ac

--- pr-23/flutter_bootstrap.js
+++ pr-23/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2484203470" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2583502612" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
