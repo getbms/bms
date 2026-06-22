37a5553d89ff51aaa94ac1a30f249f52ce5247de
hiranyasemindi <hiranyasemindi@users.noreply.github.com>
2026-06-22 20:13:48 +0000

    deploy: staging @ 37178d43824c4e60b25678087749e3aaac203ddd 37178d43824c4e60b25678087749e3aaac203ddd

--- staging/flutter_bootstrap.js
+++ staging/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2638602804" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1260268692" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
